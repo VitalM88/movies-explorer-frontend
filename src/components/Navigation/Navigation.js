5dc23d382dd3f5f9aca343a9c66d7250533039b1
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-11-02 12:22:02 +0300

    mobile navigation layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import './Navigation.css';
 import { Link } from 'react-router-dom';
 import profileIcon from '../../images/profile-icon.svg';
 
 function Navigation({ state }) {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [btnState, toogleBtnState] = useState('false');
+
+  const handleClick = () => {
+    toogleBtnState(s => !s);
+  };
+
+  const navBarState = (btnState ? "noactive" : "active");
+
+  const updateWindowWidth = () => setWindowWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  });
+
+
+
   return (
     <>
       {
@@ -17,14 +37,50 @@ function Navigation({ state }) {
       }
       {
         state === "header_nav" ? (
-          <div className="navigation navigation_nav">
-            <Link to="/movies" className="navigation__link">Фильмы</Link>
-            <Link to="/saved-movies" className="navigation__link">Сохраненные фильмы</Link>
-            <Link to="/profile" className="navigation__link navigation__link_type_btn">
-              <img className="navigation__icon" src={profileIcon} alt="Иконка"></img>
-              Аккаунт
-            </Link>
-          </div>
+          (windowWidth > 800) ? (
+            <div className="navigation navigation_nav">
+              <Link to="/movies" className="navigation__link">Фильмы</Link>
+              <Link to="/saved-movies" className="navigation__link">Сохраненные фильмы</Link>
+              <Link to="/profile" className="navigation__profile-btn">
+                <img className="navigation__icon" src={profileIcon} alt="Иконка"></img>
+                Аккаунт
+              </Link>
+            </div>
+          ) : (
+            <div>
+              {
+                (navBarState === "noactive") ? (
+                  <button 
+                    type='button' 
+                    onClick={handleClick} 
+                    className={`navigation__toogle-btn navigation__toogle-btn_state_${navBarState}`}
+                  >           
+                  </button>
+                ) : ''
+              }
+              {
+                (navBarState === "active") ? (
+                  <div className="navigation__mobile">
+                    <div className="navigation__mobile-container">
+                      <button 
+                        type='button' 
+                        onClick={handleClick} 
+                        className={`navigation__toogle-btn navigation__toogle-btn_state_${navBarState}`}
+                      >           
+                      </button>
+                      <Link to="/" className="navigation__link">Главная</Link>
+                      <Link to="/movies" className="navigation__link">Фильмы</Link>
+                      <Link to="/saved-movies" className="navigation__link">Сохраненные фильмы</Link>
+                      <Link to="/profile" className="navigation__profile-btn navigation__profile-btn_state_mobile ">
+                        <img className="navigation__icon" src={profileIcon} alt="Иконка"></img>
+                        Аккаунт
+                      </Link>
+                    </div>
+                  </div>
+                ) : ''
+              }
+            </div>
+          )  
         ) : ''
       }
     </>
